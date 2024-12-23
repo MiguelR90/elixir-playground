a636bdabbda0f9549b2c78c971f2dfbc0103b7e5
Miguel Rodriguez <marodriguez9@gmail.com>
2024-12-23 15:58:46 -0800

    anonymous function and protocols

--- learn.exs
+++ learn.exs
@@ -49,3 +49,45 @@ end
 
 IO.puts("Running main...")
 Main.run()
+
+# Anonymous function are function not attached to modules
+myfunc = fn x -> "IO str: " <> x end
+IO.puts(is_function(myfunc))
+
+1..3 |> Enum.map(fn _x -> IO.puts("inside map") end)
+# can be accomplished by Enum.each as well not sure the difference
+# 1..3 |> Enum.map(fn _x -> IO.puts("inside map") end)
+
+IO.puts(1..10 |> Enum.map(fn x -> String.Chars.to_string(x) end))
+
+# If you want to run it in a script you have to wrap it into a module
+defmodule Utils do
+  def prefix(x) do
+    # this will be computed but ignored
+    "IO str: " <> x
+    # you can do this with string interpolation as well
+    "IO str: #{x}"
+  end
+end
+
+string = Utils.prefix("hello world")
+IO.puts(string)
+
+# Protocol are a what to implement interfaces
+#
+defprotocol Utility do
+  # this spec syntax is for documentation purposes and is not needed
+  @spec type(t) :: String.t()
+  def type(value)
+end
+
+defimpl Utility, for: BitString do
+  def type(_value), do: "input was string"
+end
+
+defimpl Utility, for: Integer do
+  def type(_value), do: "input was integer"
+end
+
+IO.puts(Utility.type("hello world"))
+IO.puts(Utility.type(1234))
